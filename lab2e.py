70736ac7942874e5c23fb500ab59f0e370f67c5a
Esrak Muhmmed Fahim <emfahim@mtrx-node06pd.dcm.senecacollege.ca>
2021-09-26 14:28:50 -0400

    lab2e.py submitted

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#"Author:"Esrak Fahim"
+#"Author ID:"emfahim"
+#"Date Created:"2021/09/24"
+
+timer = 10
+ 
+
+while timer > 0:
+    print(timer)
+    timer -=1
+print('blast off!')
